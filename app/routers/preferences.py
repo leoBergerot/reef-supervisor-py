a4a9278bae4eed8126ef2ccefac2c427f2d98e81
LeoBergerot <leo.bergerot@gmail.com>
2025-01-05 12:20:28 +0100

    endpoint read preferences

--- app/routers/preferences.py
+++ app/routers/preferences.py
@@ -13,6 +13,13 @@ router = APIRouter(
 )
 
 
+@router.get("", response_model=list[PreferenceResponse])
+def read(current_user: Annotated[User, Security(get_current_user, scopes=["USER"])],
+         preference_repository: Annotated[PreferenceRepository, Depends(PreferenceRepository)]):
+    return [PreferenceResponse.model_validate(preference, from_attributes=True) for preference in
+            preference_repository.get_by_user(current_user)]
+
+
 @router.patch("/{preference_id}", response_model=PreferenceResponse,
               description="Need authentication, update preference in order to enabled or disabled parameter")
 def update(preference_id: int,
